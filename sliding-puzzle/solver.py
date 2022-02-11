d10ac0085a60e77c10ee177902ea74f2075d5ff4
Tyler Weir <tylerw@live.com>
2022-02-11 00:49:17 +0000

    Outlined the A* search.

--- sliding-puzzle/solver.py
+++ sliding-puzzle/solver.py
@@ -5,11 +5,13 @@ Uses the A* search algorithm to solve a given m x n sliding puzzle.
 Written by Tyler Weir
 02/09/2022"""
 
+from goto import goto, label
+
 class State:
     """Represents a state of a sliding puzzle."""
 
     def __init__(self, width, height, layout):
-
+        self.f_score = -1
         self.h_score = self.calc_heuristic()
         self.g_score = -1
         self.width = width
@@ -55,8 +57,9 @@ class State:
 
         return sum_moves
 
-    def is_solveable(self):
-        """Returns True if the puzzle is possible to solve. False otherwise."""
+    def __is_solveable(self):
+        """Returns True if the puzzle is possible to solve.
+        False otherwise."""
 
         # Remove the zero
         scratch = [x for x in self.state if x != 0]
@@ -77,8 +80,14 @@ class State:
 
         # Solveable if the sum of zero_row and inversions is even
         return (zero_row + inversions) % 2 == 0
+    
+    def get_moves(self):
+        """Returns a tuple of the states representing the states
+        possible after making a single legal move."""
+        #TODO: Fill in
 
     def __eq__(self, other):
+        """Overrides the built-in equals method."""
         same_inst = isinstance(other, State)
         if not same_inst:
             return False
@@ -89,6 +98,7 @@ class State:
         return same_width and same_height and same_state
 
     def __hash__(self):
+        """Overrides the built-in hash method."""
         tmp = self.state[:]
         tmp.append(self.width)
         tmp.append(self.height)
@@ -105,6 +115,72 @@ def solve(puzzle):
     defined in the state class.
     """
     state = State(len(puzzle), len(puzzle[0]), puzzle)
+
+def __solve(start_state):
+    """Private solver using A* to search through the states.
+
+    Keyword arguments:
+    start_state -- A SOLVABLE State"""
+
+    #TODO: Implement A*
+    open_list = {}
+    closed_list = {}
+
+    # Put the start node on the open list with f = h
+    open_list.update({start_state:None})
+    start_state.f_score = start_state.h_score
+
+    while len(open_list.keys()) > 0:
+        #TODO: When to update h and g and f?
+        current = __pop_smallest(open_list)
+        if current.h_score == 0:
+            # We found the solution
+            break
+
+        # Generate the sucessor states
+        moves = current.get_moves()
+
+        for move in moves:
+            #set current move cost 
+            current_cost = #TODO
+            
+            # Is the move on the open list?
+            if open_list.get(move) != None:
+                if move.g_score <= current_cost: 
+                    goto .add_to_closed_list
+            # Is the move on the closed list?
+            else if close_list.get(move) != None:
+                if move.g_score <= current_cost:
+                    goto .add_to_closed_list
+                else:
+                    # TODO: move the move from the closed list to 
+                    # the open list
+            else: 
+                # TODO Add move to the open list 
+                # TODO set move.h_score 
+            move.g_score = current_cost
+            #TODO set parent of move to current state
+
+        label .add_to_closed_list
+   # should be done
+   # TODO return the closed list?
+
+
+def __pop_smallest(dictionary):
+    """Returns the state with the smallest f_score as a tuple
+    with the parent state."""
+    items = dictionary.items()
+    lowest_state = items[0]
+
+    # Iterate through all the states in the list
+    for (state, parent) in items:
+        if state.f_score < lowest_state.f_score:
+            lowest_state = (state, parent)
+
+    # Return the lowest.
+    dictionary.pop(lowest_state[0])
+    return lowest_state
+
 
 if __name__ == "__main__":
     #myState = State(4, 4, [6, 5, 2, 0, 3, 7, 11, 4, 9, 1, 10, 8, 15, 14, 13, 12])
