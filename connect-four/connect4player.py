39699011f3862e70553b45281c0f4cc6214b5116
Tyler <tylerw@live.com>
2022-03-03 21:51:11 -0800

    "Precompute quartets and better pruning."

--- connect-four/connect4player.py
+++ connect-four/connect4player.py
@@ -28,6 +28,12 @@ class ComputerPlayer:
             self.opponent_id = 1
 
         self.difficulty = difficulty_level
+        print(self.difficulty)
+        
+        # Used for fast quartet score lookup
+        print("starting quartet generation")
+        self.quartet_scores = precompute_quartets()
+        print("finished")
 
     def pick_move(self, rack):
         """
@@ -47,12 +53,18 @@ class ComputerPlayer:
         beta = float('inf')
 
         # Use minimax to score the possbible moves
+        # This first layer is a max player
         for move in self.get_moves(rack, self.player_id):
-            score, alpha, beta = self.minimax(move, self.difficulty, False, alpha, beta)
+            score = self.minimax(move, self.difficulty, False, alpha, beta)
 
             if score > best_move_score:
                 best_move_score = score
                 best_move = move
+
+            alpha = max(alpha, best_move_score)
+            if beta <= alpha:
+                print("Cut a whole branch!")
+                break
 
         # This means the bot will lose
         if best_move == None:
@@ -75,30 +87,32 @@ class ComputerPlayer:
 
         # Return score if this is the bottom.
         if depth == 0:
-            return (self.calc_heuristic(board_state), alpha, beta)
+            return self.calc_heuristic(board_state)
         
         # Evaluate the max player
         if max_player:
             max_score = -float('inf')
             for move in self.get_moves(board_state, self.player_id):
-                score = self.minimax(move, depth-1, False, alpha, beta)[0]
+                score = self.minimax(move, depth-1, False, alpha, beta)
                 max_score = max(score, max_score)
                 alpha = max(alpha, max_score)
                 if beta <= alpha:
+                    print("max sliced a branch")
                     break
-            return (max_score, alpha, beta)
+            return max_score
         
         # Evaluate the min player
         else:
             min_score = float('inf')
             for move in self.get_moves(board_state, self.opponent_id):
-                score = self.minimax(move, depth-1, True, alpha, beta)[0]
+                score = self.minimax(move, depth-1, True, alpha, beta)
                 min_score = min(score, min_score)
                 beta = min(beta, min_score)
                 if beta <= alpha:
+                    print("min sliced a branch")
                     break
 
-            return (min_score, alpha, beta)
+            return min_score
 
     def calc_heuristic(self, board_state):
         """Calculates the estimated score of a given board. Positive
@@ -110,108 +124,47 @@ class ComputerPlayer:
         height = len(board_state[0])
 
         # Iterate through each space on the board
-        for i, _ in enumerate(board_state):
-            for j, _ in enumerate(board_state[i]):
+        for i in range(width-3):
+            for j in range(height):
 
                 # Check rightward quartet
                 if i+3 < width:
-                    quartet = [board_state[i][j],
+                    quartet = (board_state[i][j],
                                board_state[i+1][j],
                                board_state[i+2][j],
-                               board_state[i+3][j]]
+                               board_state[i+3][j])
 
-                    score += self.__calc_quartet_score(quartet)
+                    score += self.quartet_scores.get(quartet)[self.player_id-1]
 
                 # Check upward quartet
                 if j+3 < height:
-                    quartet = [board_state[i][j],
+                    quartet = (board_state[i][j],
                                board_state[i][j+1],
                                board_state[i][j+2],
-                               board_state[i][j+3]]
+                               board_state[i][j+3])
 
-                    score += self.__calc_quartet_score(quartet)
+                    score += self.quartet_scores.get(quartet)[self.player_id-1]
 
                 # Check up-right quartet
                 if i+3 < width and j+3 < height:
-                    quartet = [board_state[i][j],
+                    quartet = (board_state[i][j],
                                board_state[i+1][j+1],
                                board_state[i+2][j+2],
-                               board_state[i+3][j+3]]
+                               board_state[i+3][j+3])
 
-                    score += self.__calc_quartet_score(quartet)
+                    score += self.quartet_scores.get(quartet)[self.player_id-1]
 
                 # Check down-right quartet
                 if(i+3 < width and j-3 >= 0):
-                    quartet = [board_state[i][j],
+                    quartet = (board_state[i][j],
                                board_state[i+1][j-1],
                                board_state[i+2][j-2],
-                               board_state[i+3][j-3]]
+                               board_state[i+3][j-3])
 
-                    score += self.__calc_quartet_score(quartet)
+                    score += self.quartet_scores.get(quartet)[self.player_id-1]
+
         return score
 
-    def __calc_quartet_score(self, quartet):
-        """Calculates the score contribution of a given quartet."""
-        # For each quartet check:
-        # - contains at least one disc of each color = 0
-        # - contains 4 discs of the same color = +/- inf
-        # - contains 3 discs of the same color and one empty +/- 100
-        # - contains 2 discs of the same color (2 empties) +/- 10
-        # - contains 1 disc (and 3 empties) is worth +/- 1
-
-        # Check at least one disc of each color
-        if 1 in quartet and 2 in quartet:
-            return 0
-
-        # Check 4 discs of the same color
-        if quartet[0] == quartet[1] == quartet[2] == quartet[3]:
-            if quartet[0] == self.player_id:
-                return float('inf')
-            if quartet[0] != self.player_id and quartet[0] != 0:
-                return -float('inf')
-
-        sum_1 = sum( x == 1 for x in quartet)
-        sum_2 = sum( x == 2 for x in quartet)
-
-        if self.player_id == 1:
-            if sum_1 == 4:
-                return float('inf')
-            if sum_1 == 3:
-                return 100
-            if sum_1 == 2:
-                return 10
-            if sum_1 == 1:
-                return 1
-
-            if sum_2 == 4:
-                return -float('inf')
-            if sum_2 == 3:
-                return -100
-            if sum_2 == 2:
-                return -10
-            if sum_2 == 1:
-                return -1
-
-        if self.player_id == 2:
-            if sum_2 == 4:
-                return float('inf')
-            if sum_2 == 3:
-                return 100
-            if sum_2 == 2:
-                return 10
-            if sum_2 == 1:
-                return 1
-
-            if sum_1 == 4:
-                return -float('inf')
-            if sum_1 == 3:
-                return -100
-            if sum_1 == 2:
-                return -10
-            if sum_1 == 1:
-                return -1
-
-        return 0
 
     def get_moves(self, board_state, player):
         """Returns a list descibing the states of the board after
@@ -245,3 +198,88 @@ class ComputerPlayer:
 
         # extract the boards and return
         return [c for (a, b, c) in moves]
+
+def precompute_quartets():
+    """Computes the scores of all quartets and returns a dictionary 
+    mapping the quartet to a tuple where the first index is the 
+    score for player 1 and the second index is the score for player 2."""
+
+    # Make list of all possible quartets
+    a = [0, 1, 2]
+    b = [0, 1, 2]
+    c = [0, 1, 2]
+    d = [0, 1, 2]
+
+    all_quartets = [(i, j, k, l) for i in a
+                                 for j in b
+                                 for k in c
+                                 for l in d]
+
+    # Score each quartet and store scores in dict
+    score_dict = {}
+    for quartet in all_quartets:
+        score_dict.update({quartet: calc_quartet_score(quartet)})
+
+    return score_dict
+
+def calc_quartet_score(quartet):
+    """Calculates the score contribution of a given quartet. Returns
+    a tuple where the first element is player 1 score for the quartet 
+    and the second element is player 2 score for the quartet."""
+    # For each quartet check:
+    # - contains at least one disc of each color = 0
+    # - contains 4 discs of the same color = +/- inf
+    # - contains 3 discs of the same color and one empty +/- 100
+    # - contains 2 discs of the same color (2 empties) +/- 10
+    # - contains 1 disc (and 3 empties) is worth +/- 1
+
+    # Check at least one disc of each color
+    if 1 in quartet and 2 in quartet:
+        return (0, 0)
+    
+    # Calculate the score for each player
+    player1_score = 0
+    player2_score = 0
+
+    sum_1 = sum( x == 1 for x in quartet)
+    sum_2 = sum( x == 2 for x in quartet)
+
+    # Calc player1 score
+    if sum_1 == 4:
+        player1_score = float('inf')
+    if sum_1 == 3:
+        player1_score = 100
+    if sum_1 == 2:
+        player1_score = 10
+    if sum_1 == 1:
+        player1_score = 1
+
+    if sum_2 == 4:
+        player1_score = -float('inf')
+    if sum_2 == 3:
+        player1_score = -100
+    if sum_2 == 2:
+        player1_score = -10
+    if sum_2 == 1:
+        player1_score = -1
+
+    # Calc player2 score
+    if sum_2 == 4:
+        player2_score = float('inf')
+    if sum_2 == 3:
+        player2_score = 100
+    if sum_2 == 2:
+        player2_score = 10
+    if sum_2 == 1:
+        player2_score = 1
+
+    if sum_1 == 4:
+        player2_score = -float('inf')
+    if sum_1 == 3:
+        player2_score = -100
+    if sum_1 == 2:
+        player2_score = -10
+    if sum_1 == 1:
+        player2_score = -1
+
+    return (player1_score, player2_score)
