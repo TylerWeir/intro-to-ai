c9436f59d5e312d35615d45a999a31ec531d22cd
Tyler Weir <tylerw@live.com>
2022-03-28 22:58:42 +0000

    Finished calc information gain.

--- decision-tree/tree-inducer.py
+++ decision-tree/tree-inducer.py
@@ -32,21 +32,56 @@ class DecisionTree:
         # Second, Calculate the information gain of splitting based
         # on each feature
         
-        #TODO only first question right now
-        print("calc info gain from first feature")
-        gain = self.__calc_info_gain(data, 0)
+        best_feature = 0
+        greatest_gain = -1
+       
+        for i, _ in enumerate(data[0][2]):
+            gain = self.__calc_info_gain(data, i)
+            
+            print(f'issue {i} has gain {gain}')
 
+            if gain > greatest_gain:
+                greatest_gain = gain
+                best_feature = i
+
+        print(f'best feature is: {best_feature}')
+        
         # Third, Choose the single best feature and divide the data
         # set into two or more discrete groups.
 
+        yes_votes = [(n, l, v) for (n, l, v) in data if v[best_feature] == '+']
+        no_votes = [(n, l, v) for (n, l, v) in data if v[best_feature] == '-']
+        ab_votes = [(n, l, v) for (n, l, v) in data if v[best_feature] == '.']
+
         # Fourth, if a subgroup is not uniformly labeled, recurse
+
+        if self.__is_uniform(yes_votes):
+            # make a leaf node
+            print("yes leaf")
+        els
+            # recurse on the yes voters
+            self.build_tree_from_data(yes_votes)
+
+        if self.__is_uniform(no_votes):
+            # make a leaf node
+            print("no leaf")
+        else: 
+            # recurse on the no voters
+            self.build_tree_from_data(no_votes)
+
+        if self.__is_uniform(ab_votes):
+            # make a leaf node
+            print("ab leaf")
+        else:
+            # recurse on the ab voters
+            self.build_tree_from_data(ab_votes)
 
     def __calc_total_entropy(self, data):
         """Calculates the entropy of the entire dataset."""
         total = len(data)
         tmp = [party for (name, party, votes) in data]
         num_d = tmp.count('D')
-        num_r = total-num_d
+        num_r = tmp.count('R')
 
         prob_d = num_d/total
         prob_r = num_r/total
@@ -54,10 +89,20 @@ class DecisionTree:
         entropy = -prob_d*math.log2(prob_d) -prob_r*math.log2(prob_r)
         return entropy
 
-
     def calc_accuracy(self):
         """Calculates the estimated accuracy of the decision tree."""
         return None
+    
+    def __is_uniform(self, data):
+        """Returns true if the data set is uniformly labeled."""
+
+        test = data[0][1]
+
+        for item in data:
+            if item[0][1] != test:
+                return False
+        
+        return True
 
     def print_tree(self):
         """Prints a text representation of the decision tree."""
@@ -70,31 +115,37 @@ class DecisionTree:
         # Trim data to just the party and the issue
         data = [(p, v[issue]) for (r, p, v) in data]
 
-        num_r_yes = data.count(('R', '+'))
+        len_data = len(data)
         num_yes = [v for (p, v) in data].count('+')
+        num_no = [v for (p, v) in data].count('-')
+        num_ab = [v for (p, v) in data].count('.')
         
         entropy_yes  = self.__calc_entropy(data, '+')
-        entropy_other = self.__calc_entropy(data, '-.')
+        entropy_no = self.__calc_entropy(data, '-')
+        entropy_ab = self.__calc_entropy(data, '.')
+
+        gain = total_entropy
+        gain -= num_yes/len_data * entropy_yes
+        gain -= num_no/len_data * entropy_no
+        gain -= num_ab/len_data * entropy_ab
+        
+        return gain
+
 
     def __calc_entropy(self, data, choice):
         """Calculates the entropy of asking about choice."""
-        num_yes = [v for (p, v) in data].count('+')
-        num_r_yes = data.count(('R', '+'))
-        num_d_yes = data.count(('D', '+'))
+        num_choice = [v for (p, v) in data].count(choice)
+        num_r_choice = data.count(('R', choice))
+        num_d_choice = data.count(('D', choice))
 
-        if choice == '+':
-            tmp = -(num_r_yes/num_yes) * math.log2(num_r_yes/num_yes)
-            tmp += -(num_d_yes/num_yes) * math.log2(num_d_yes/num_yes)
-            return tmp
+        if num_d_choice == 0:
+            return 0
 
-        # Otherwise calculating the entropy of no and abstain
-        num_r = [p for (p, v) in data].count('R')
-        num_r_no = num_r - num_r_yes
-        num_no = len(data) - num_yes
-        num_d_no = num_r - num_r_no
+        if num_r_choice == 0:
+            return 0
 
-        tmp = -(num_r_no/num_no) * math.log2(num_r_no/num_no)
-        tmp += -(num_d_no/num_no) * math.log2(num_d_no/num_no)
+        tmp = -(num_r_choice/num_choice) * math.log2(num_r_choice/num_choice)
+        tmp += -(num_d_choice/num_choice) * math.log2(num_d_choice/num_choice)
         return tmp
 
     class Node:
@@ -134,7 +185,6 @@ if __name__ == '__main__':
 
     # Split each line by white space
     data = [line.split() for line in data]
-    print("finished reading the file")
 
     #TODO only build the tree from some of the data
     # Create the Decision Tree
