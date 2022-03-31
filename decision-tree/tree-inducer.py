c542ac112ca514de0d72798da78a5d0cb8b5ea86
Tyler Weir <tylerw@live.com>
2022-03-31 19:06:42 +0000

    Refactored the decision tree algorithm

--- decision-tree/tree-inducer.py
+++ decision-tree/tree-inducer.py
@@ -14,46 +14,49 @@ import sys
 import math
 
 class Node:
-        """Represents a Node in the decision tree. Terminal Nodes
-        have no children and thus must contain a label."""
+    """Represents a Node in the decision tree. Terminal Nodes
+    have no children and thus must contain a label."""
 
-        def __init__(self, vote, value, parent=None):
-            """Creates a new node."""
-            self.parent = parent
-            self.children = [] 
-            self.vote = vote
-            self.value = value
+    def __init__(self, vote, value, parent=None):
+        """Creates a new node."""
+        self.parent = parent
+        self.children = []
+        self.vote = vote
+        self.value = value
 
-        def add_child(self, child):
-            """Adds a child node to the current child list"""
-            self.children.append(child)
+    def add_child(self, child):
+        """Adds a child node to the current child list"""
+        self.children.append(child)
 
-        def get_children(self):
-            """Returns a list containing the children of the node."""
-            return self.children
+    def get_children(self):
+        """Returns a list containing the children of the node."""
+        return self.children
 
-        def get_value(self):
-            """Returns the value of the Node."""
-            return self.value
+    def get_value(self):
+        """Returns the value of the Node."""
+        return self.value
 
-        def is_terminal(self):
-            """Returns true if the Node is a terminal Node."""
-            return len(self.children) == -1
+    def is_terminal(self):
+        """Returns true if the Node is a terminal Node."""
+        return len(self.children) == -1
 
-        def __str__(self):
-            return f"(NODE: {self.vote} {self.value})"
+    def __str__(self):
+        return f"(NODE: {self.vote} {self.value})"
 
 
 class DecisionTree:
     """A Decision Tree classifier."""
 
-    def __init__(self):
+    def __init__(self, data):
         """Default constructor for making a new decision tree."""
         self.root = Node("Root", "ISSUE")
+        self.build_tree_from_data(data, self.root)
 
     def build_tree_from_data(self, data, node):
         """Builds the decision tree from the given tab deliminated
         file."""
+
+        #TODO PLEASE PLEASE PLEASE REFACTOR THIS!!
 
         ### Decision Tree Algorithm
 
@@ -62,32 +65,16 @@ class DecisionTree:
 
         # Second, Calculate the information gain of splitting based
         # on each feature
-        
-        best_feature = 0
-        greatest_gain = -1
-       
-        for i, _ in enumerate(data[0][2]):
-            gain = self.__calc_info_gain(data, i)
 
-            if gain > greatest_gain:
-                greatest_gain = gain
-                best_feature = i
-
-        node.value = f"issue {best_feature}"
-        
-        if greatest_gain == 0:
+        best_feature = self.__calc_best_feature(data)
+        if best_feature == -1:
+            # Greatest information gain was 0
             #TODO
             node.value = 'R'
             return
-        
-        # Checks:
+        else:
+            node.value = f"issue {best_feature}"
 
-        # IF both Democrat and Republican with same voting record
-        # create a leaf that classifies them as the majority
-
-        # If a branch has zero reps classify based on the majority at its parent
-        # keep going up the tree until you have a majority
-        
         # Third, Choose the single best feature and divide the data
         # set into two or more discrete groups.
 
@@ -96,80 +83,58 @@ class DecisionTree:
         ab_votes = [(n, l, v) for (n, l, v) in data if v[best_feature] == '.']
 
         # Fourth, if a subgroup is not uniformly labeled, recurse
-        """
-        if len(yes_votes) != 0:
-            yes_node = Node('+', "Issue", parent=node)
-            node.add_child(yes_node)
-                
-            if not self.__is_uniform(yes_votes):
-                self.build_tree_from_data(yes_votes, yes_node)            
+        self.__handle_subgroup(yes_votes, node, '+')
+        self.__handle_subgroup(no_votes, node, '-')
+        self.__handle_subgroup(ab_votes, node, '.')
 
-        if len(no_votes) != 0:
-            no_node = Node('-', "Issue", parent=node)
-            node.add_child(no_node)
+    def __calc_best_feature(self, data):
+        """Returns the feature index that results in the greatest information gain.
+        if the greatest possible gain is 0 then -1 is returned."""
 
-            if not self.__is_uniform(no_votes):
-                self.build_tree_from_data(no_votes, no_node)            
-        
-        if len(ab_votes) != 0:
-            ab_node = Node('.', "Issue", parent=node)
-            node.add_child(ab_node)
+        best_feature = -1
+        greatest_gain = 0
 
-            if not self.__is_uniform(ab_votes):
-                self.build_tree_from_data(ab_votes, ab_node)            
-        """
-        if len(yes_votes) != 0:
-            if self.__is_uniform(yes_votes):
+        for i, _ in enumerate(data[0][2]):
+            gain = self.__calc_info_gain(data, i)
+
+            if gain > greatest_gain:
+                greatest_gain = gain
+                best_feature = i
+
+        return best_feature
+
+
+    def __handle_zero_gain(self, data, node):
+        """Handles if there is zero gain"""
+        return None
+
+    def __handle_subgroup(self, subgroup, node, choice):
+        """Handles the subgrouping from the decision tree."""
+
+        # IF both Democrat and Republican with same voting record
+        # create a leaf that classifies them as the majority
+
+        # If a branch has zero reps classify based on the majority at its parent
+        # keep going up the tree until you have a majority
+
+        if len(subgroup) != 0:
+            if self.__is_uniform(subgroup):
                 # make a leaf node
-                yes_node = Node('+', 'R', parent=node)
-                node.add_child(yes_node)
+                label = subgroup[0][1]
+                leaf_node = Node(choice, label, parent=node)
+                node.add_child(leaf_node)
 
             else:
-                yes_node = Node('+', 'Some Issue', parent=node)
-                node.add_child(yes_node)
+                branch_node = Node(choice, 'Some Issue', parent=node)
+                node.add_child(branch_node)
 
-                # recurse on the yes voters
-                self.build_tree_from_data(yes_votes, yes_node)
+                # recurse on the subgroup 
+                self.build_tree_from_data(subgroup, branch_node)
         else:
             # There are zero reps
-            yes_node = Node('+', 'R', parent=node)
-            node.add_child(yes_node)
-
-        if len(no_votes) != 0:
-            if self.__is_uniform(no_votes):
-                # make a leaf node
-                no_node = Node('-', 'R', parent=node)
-                node.add_child(no_node)
-
-            else: 
-                # recurse on the no voters
-                no_node = Node('-', 'Some Issue', parent=node)
-                node.add_child(no_node)
-
-                self.build_tree_from_data(no_votes, no_node)
-        else:
-            # There are zero reps
-            # TODO:
-            # currently just assigning 'R'
-            no_node = Node('-', 'R', parent=node)
-            node.add_child(no_node)
-            
-
-        if len(ab_votes) != 0:
-            if self.__is_uniform(ab_votes):
-                # make a leaf node
-                ab_node = Node('.', 'R', parent=node)
-                node.add_child(ab_node)
-            else:
-                ab_node = Node('.', 'Some Issue', parent=node)
-                node.add_child(ab_node)
-
-                # recurse on the ab voters
-                self.build_tree_from_data(ab_votes, ab_node)
-        else: 
-            # There are zero reps
-            ab_node = Node('.', 'R', node)
-            node.add_child(ab_node)
+            #TODO: classify based on majority of parent
+            leaf_node = Node(choice, 'R', parent=node)
+            node.add_child(leaf_node)
 
     def __calc_total_entropy(self, data):
         """Calculates the entropy of the entire dataset."""
@@ -202,10 +167,10 @@ class DecisionTree:
         
         return True
 
-    def __persistant_get_majority(self, node):
+    def __persistant_calc_majority(self, node):
         pass
 
-    def __get_majority(self, data):
+    def __calc_majority(self, data):
         """Returns the majority party."""
         D = 0
         R = 0
@@ -224,27 +189,31 @@ class DecisionTree:
             return None
 
         
-    def print_tree(self, root, depth):
+    def print_tree(self):
         """Prints a text representation of the decision tree."""
+        def __print_tree(root, depth):
+            """Recursive tree printing function."""
 
-        if depth == 0:
-            print(f'{root.value}:')
+            if depth == 0:
+                print(f'{root.value}:')
 
-            # Recurse on children
-            for child in root.children:
-                self.print_tree(child, depth + 1)
+                # Recurse on children
+                for child in root.children:
+                    __print_tree(child, depth + 1)
+            else:
+                # Print the node
+                spaces = ""
+                for i in range(depth):
+                    spaces += "  "
 
-        else:
-            # Print the node
-            spaces = ""
-            for i in range(depth):
-                spaces += "  "
+                print(spaces + root.vote + " " + root.value)
 
-            print(spaces + root.vote + " " + root.value)
+                # Recurse on children
+                for child in root.children:
+                    __print_tree(child, depth + 1)
 
-            # Recurse on children
-            for child in root.children:
-                self.print_tree(child, depth + 1)
+        # Enter the recurisve printer
+        __print_tree(self.root, 0)
 
 
     def __calc_info_gain(self, data, issue):
@@ -305,74 +274,7 @@ if __name__ == '__main__':
 
     #TODO only build the tree from some of the data
     # Create the Decision Tree
-    my_tree = DecisionTree()
-    
-    my_tree.build_tree_from_data(data, my_tree.root)
+    my_tree = DecisionTree(data)
 
-    """
-    print(len(my_tree.root.children))
-    
-    print("ROOT Children: ")
-    for child in my_tree.root.children:
-        print(child)
-
-    """
-    
     # Display the tree
-    my_tree.print_tree(my_tree.root, 0)
-
-
-""" if len(yes_votes) != 0:
-            if self.__is_uniform(yes_votes):
-                # make a leaf node
-                yes_node = self.Node('+', 'R', parent=node)
-                node.children.append(yes_node)
-
-            else:
-                yes_node = self.Node('+', 'Some Issue', parent=node)
-                node.children.append(yes_node)
-
-                # recurse on the yes voters
-                self.build_tree_from_data(yes_votes, yes_node)
-        else:
-            # There are zero reps
-            yes_node = self.Node('+', 'R', parent=node)
-            node.children.append(yes_node)
-
-        if len(no_votes) != 0:
-            if self.__is_uniform(no_votes):
-                # make a leaf node
-                no_node = self.Node('-', 'R', parent=node)
-                node.children.append(no_node)
-
-            else: 
-                # recurse on the no voters
-                no_node = self.Node('-', 'Some Issue', parent=node)
-                node.children.append(no_node)
-
-                self.build_tree_from_data(no_votes, no_node)
-        else:
-            # There are zero reps
-            # TODO:
-            # currently just assigning 'R'
-            no_node = self.Node('-', 'R', parent=node)
-            node.children.append(no_node)
-            
-
-        if len(ab_votes) != 0:
-            if self.__is_uniform(ab_votes):
-                # make a leaf node
-                ab_node = self.Node('.', 'R', parent=node)
-                node.children.append(ab_node)
-            else:
-                ab_node = self.Node('.', 'Some Issue', parent=node)
-                node.children.append(ab_node)
-
-                # recurse on the ab voters
-                self.build_tree_from_data(ab_votes, ab_node)
-        else: 
-            # There are zero reps
-            ab_node = self.Node('.', 'R', node)
-            node.children.append(ab_node)
-
-        print(f"{node} has {len(node.children)} children")"""
+    my_tree.print_tree()
